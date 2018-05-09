a80d105cd452779b3a2d8afaee1c88c5fd3ccb15
yomZsamora <samora.y@adzumi.co.ke>
2018-05-09 14:32:11 +0300

    Added more counties.

--- src/main/java/SelectCounty.java
+++ src/main/java/SelectCounty.java
@@ -9,7 +9,7 @@ public class SelectCounty{
     	Console myConsole = System.console();
       System.out.println("Lets play the classic Hangman. You can either play by yourself or with a friend. Press Enter to continue:");
       myConsole.readLine();
-    	String[] counties = {"nyamira", "bungoma", "kakamega", "nyandarua", "marsabit", "mandera", "garissa", "kilifi", "kwale", "mombasa"};
+    	String[] counties = {"nyamira", "bungoma", "kakamega", "nyandarua", "marsabit", "mandera", "garissa", "kilifi", "kwale", "mombasa", "migori", "kiambu", "siaya", "kajiado", "kericho", "laikipia", "samburu", "kirinyaga", "lamu", "isiolo"};
     	Random rand = new Random(); 
     	int x = rand.nextInt(counties.length); // Generates random number according to array's length
     	selectedCounty = counties[x]; // Assigns value
